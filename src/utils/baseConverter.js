442f0f39519e7e3667bf636f416fa7554c8ecebe
Roman Fairushyn <fyrushin_roman@yahoo.com>
2023-03-08 00:34:04 -0800

    publish web
    publish web

--- src/utils/baseConverter.js
+++
@@ -1,5 +0,0 @@
-function baseConverter(str) {
-    return 'data:image/png;base64,' + str;
-}
-
-export default baseConverter
--- src/utils/stripHtml.js
+++
@@ -1,5 +0,0 @@
-function stripHtml(str) {
-    return str.replace(/(<([^>]+)>)/gi, "").replace(/&nbsp;/g, '').replace(/&amp;/g, '');
-}
-
-export default stripHtml
